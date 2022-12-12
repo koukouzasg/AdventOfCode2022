9d3e9bc879382a57d4828391ae3ff45c9b40f620
koukouzasg <koukouzasg@hotmail.gr>
2022-12-13 01:46:53 +0200

    day 6 solutions

---
+++ day6/tuner.py
@@ -0,0 +1,13 @@
+def find_packet(stream, length: int) -> int:
+    for i in range(len(stream)):
+        packet = set()
+        for x in range(length):
+            packet.add(stream[i+x])
+        if len(packet) == length:
+            return i + length
+
+
+with open("input.txt") as file:
+    stream = file.read()
+
+print(find_packet(stream, 14))
